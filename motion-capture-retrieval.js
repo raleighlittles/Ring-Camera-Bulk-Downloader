c285a1112d8aedb54e10ec6714d3e7d0a611f2d2
Raleigh Littles III <raleighlittles@gmail.com>
2022-03-11 15:02:15 -0800

    correctly prints locations now, needed to use dumb async

--- motion-capture-retrieval.js
+++ motion-capture-retrieval.js
@@ -1,27 +1,12 @@
-
-
-// Ring API Setup section
 import { RingApi } from 'ring-client-api'
 
+let ringApi = new RingApi({
+       // Replace with your refresh token
+       "refreshToken": "eyJhbGciOiJIUzUxMiIsImprdSI6Ii9vYXV0aC9pbnRlcm5hbC9qd2tzIiwia2lkIjoiYzEyODEwMGIiLCJ0eXAiOiJKV1QifQ.eyJpYXQiOjE2NDY5NDkwMjIsImlzcyI6IlJpbmdPYXV0aFNlcnZpY2UtcHJvZDp1cy1lYXN0LTE6M2Q3ZDcxOWYiLCJyZWZyZXNoX2NpZCI6InJpbmdfb2ZmaWNpYWxfYW5kcm9pZCIsInJlZnJlc2hfc2NvcGVzIjpbImNsaWVudCJdLCJyZWZyZXNoX3VzZXJfaWQiOjcwNzU1NTU4LCJybmQiOiJUTm5vQmMtNkNuS2dYZyIsInNlc3Npb25faWQiOiIzNGE1MjA5MS0yZTUwLTQxM2EtODJkNi00YzU2MzQxOTJjN2UiLCJ0eXBlIjoicmVmcmVzaC10b2tlbiJ9.7i7TGO2CXDVX3Ey8_bp3GhDup5R0_aquPW--VEs63yOU4Yp7mwEAn3MhthRvngUhAZFmhW6Jvx01gF4KTTpyUA"
+     });
 
-async function fetchAllMotionEvents() {
+let locations = ringApi.getLocations().then((value) => {
+  console.log(value);
+});
 
-  const { env } = process,
-  ringApi = new RingApi({
-    // Replace with your refresh token
-    "refreshToken": "eyJhbGciOiJIUzUxMiIsImprdSI6Ii9vYXV0aC9pbnRlcm5hbC9qd2tzIiwia2lkIjoiYzEyODEwMGIiLCJ0eXAiOiJKV1QifQ.eyJpYXQiOjE2NDYzNzY5MjEsImlzcyI6IlJpbmdPYXV0aFNlcnZpY2UtcHJvZDp1cy1lYXN0LTE6NjAzZjIzNzgiLCJyZWZyZXNoX2NpZCI6InJpbmdfb2ZmaWNpYWxfYW5kcm9pZCIsInJlZnJlc2hfc2NvcGVzIjpbImNsaWVudCJdLCJyZWZyZXNoX3VzZXJfaWQiOjcwNzU1NTU4LCJybmQiOiJSSnBxcnNtX0dKNExqUSIsInNlc3Npb25faWQiOiI1MjUyMzdmZC01NmQ1LTRhZjgtYTgyMi1kNjFkMjJmMGU3YjQiLCJ0eXBlIjoicmVmcmVzaC10b2tlbiJ9.iGPCDOEPOlyODmZPpEDy1sin6vBgeg4ANhWYxrDfVPOQWcYSrI3qzw7Q5CIpfP1ZDSrXOfwMesesuAd-IbXBnA",
-  }),
-  locations = await ringApi.getLocations(),
-  location = locations[0],
-  cameras = await ringApi.getCameras(),
-  camera = cameras[0]
-
-// Locations API
-location.onConnected.subscribe((connected) => {
-  const state = connected ? 'Connected' : 'Connecting'
-  console.log(`${state} to location ${location.name} - ${location.id}`)
-})
-
-}
-
-fetchAllMotionEvents().catch();
+ringApi.disconnect()
